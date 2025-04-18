2ec8eefa7922e70052d1fa9156f414865f360dad
Malith Iroshan <99582850+IT21229084@users.noreply.github.com>
2025-04-18 13:11:35 +0530

    add values

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-
 set -e
 
+REGION="eu-north-1"               # 🔁 Update if needed
+CLUSTER_NAME="product-cluster"  # 🔁 Replace with your actual cluster name
+
 echo "⚙️ Switching to correct cluster..."
-aws eks --region <your-region> update-kubeconfig --name <your-cluster-name>
+aws eks --region "$REGION" update-kubeconfig --name "$CLUSTER_NAME"
 
 echo "🚀 Applying K8s resources..."
 kubectl apply -k ./k8s
